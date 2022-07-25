65d4cb5205c6edf5608d61c95acb855ce433ad84
حمدان مولانا <99571600+Hamdanbotz@users.noreply.github.com>
2022-07-25 14:58:43 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,7 +14,7 @@ global.APIKeys = {
 
 // setting 
 global.autoread = false // auto read pesan / message
-global.autobio = true 
+global.autobio = false
 
 //language
 //Available in indonesia , english & spanyol 
@@ -24,23 +24,23 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
+global.botname = "GawrGurakawai-Mdོ" //namabot kalian
+global.ownername= "حمدان مولانا" //nama kalian
 global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
 global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
 global.github = "https://zeeone-ofc.github.io/" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.email = "hamdanmaulana428@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["622150996855","6285342106390"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.premium = ['6285691136947'] //premium user
+global.owner = ["622150996855","6285691136947"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6285691136947" // nomor wa kalian
+global.ownernomerr = "+6285691136947" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = '© GawrGurakawai-Mdོ' //sticker wm ubah
+global.author = 'Di Buat Oleh حمدان مولانا' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 
